709437e5d7813901242b7cdca86fb5cb80906c49
aimeiyijia <1456723200@qq.com>
2021-08-15 16:49:29 +0800

    删除无用代码

--- src/components/modules/input.tsx
+++ src/components/modules/input.tsx
@@ -17,7 +17,7 @@ export default class InputPlus extends Vue {
     const slots = []
     for (let slot in scopedSlots) {
       // el-input内部使用了 v-if="$slots.[slotName]"来判断是否有插槽
-      // 因此这一步是骗它有插槽，然后再用scopedSlots来实现自定义渲染函数
+      // 因此这一步是骗它有插槽，然后再用scopedSlots来实现自定义渲染函数渲染插槽内容
       slots.push({ name: slot, value: [h('template')] })
     }
     return (
@@ -29,9 +29,6 @@ export default class InputPlus extends Vue {
         {...{ props: this.$attrs, on: this.$listeners }}
         {...{ scopedSlots }}
       >
-        {/* <template slot="prepend">.com</template>
-        <template slot="append">.com</template> */}
-        {/* {...slots} */}
         {slots.map(o => {
           return <template slot={o.name}>{o.value}</template>
         })}
