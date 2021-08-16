68e96d64ddc81c7390bee0ead14b1c08a1c7d4a7
aimeiyijia <1456723200@qq.com>
2021-08-16 17:38:50 +0800

    完善select组件

--- src/app.vue
+++ src/app.vue
@@ -16,51 +16,81 @@ export default {
 
       // 表单项配置
       options: [
-        {
-          // 表单项渲染类型 必需
-          type: 'Input',
-          // 表单是否隐藏 默认为false（控制的是el-form-item）
-          hidden: false,
-          // 表单项的配置项
-          attrs: {
-            type: 'text',
-            // 表单项绑定的值（字段名） 必需
-            field: 'name',
-            // 初始值
-            value: '',
-          },
-          // 表单项事件
-          on: {
-            blur: () => {
-              console.log('失去焦点')
-            },
-            input: () => {
-              console.log('input事件')
-            },
-          },
-          // 插槽
-          scopedSlots: {
-            prefix: () => {
-              return '表单内头部内容'
-            },
-            suffix: () => {
-              return '表单内尾部内容'
-            },
-            prepend: () => {
-              return <div>表单前置插槽</div>
-            },
-            append: () => {
-              return <div>表单后置插槽</div>
-            },
-          },
-          // el-form-item配置项 可选
-          config: {
-            // vue ref属性 默认为field 利用此属性来查找某一组件
-            ref: 'name',
-            label: 'input：',
-            // labelWidth: '120px',
-          },
-        },
+        // {
+        //   // 表单项渲染类型 必需
+        //   type: 'Input',
+        //   // 表单是否隐藏 默认为false（控制的是el-form-item）
+        //   hidden: false,
+        //   // 表单项的配置项
+        //   attrs: {
+        //     type: 'text',
+        //     // 表单项绑定的值（字段名） 必需
+        //     field: 'name',
+        //     // 初始值
+        //     value: '',
+        //   },
+        //   // 表单项事件
+        //   on: {
+        //     blur: () => {
+        //       console.log('失去焦点')
+        //     },
+        //     input: () => {
+        //       console.log('input事件')
+        //     },
+        //   },
+        //   // 插槽
+        //   scopedSlots: {
+        //     prefix: () => {
+        //       return '表单内头部内容'
+        //     },
+        //     suffix: () => {
+        //       return '表单内尾部内容'
+        //     },
+        //     prepend: () => {
+        //       return <div>表单前置插槽</div>
+        //     },
+        //     append: () => {
+        //       return <div>表单后置插槽</div>
+        //     },
+        //   },
+        //   // el-form-item配置项 可选
+        //   config: {
+        //     // vue ref属性 默认为field 利用此属性来查找某一组件
+        //     ref: 'name',
+        //     label: 'input：',
+        //     // labelWidth: '120px',
+        //   },
+        // },
+        // {
+        //   // 表单项渲染类型 必需
+        //   type: 'Autocomplete',
+        //   // 表单是否隐藏 默认为false（控制的是el-form-item）
+        //   hidden: false,
+        //   // 表单项的配置项
+        //   attrs: {
+        //     type: 'text',
+        //     // 表单项绑定的值（字段名） 必需
+        //     field: 'name',
+        //     // 初始值
+        //     value: '',
+        //   },
+        //   // 表单项事件
+        //   on: {
+        //     blur: () => {
+        //       console.log('失去焦点')
+        //     },
+        //     input: () => {
+        //       console.log('input事件')
+        //     },
+        //   },
+        //   // el-form-item配置项 可选
+        //   config: {
+        //     // vue ref属性 默认为field 利用此属性来查找某一组件
+        //     ref: 'name',
+        //     label: 'autocomplete：',
+        //     labelWidth: '120px',
+        //   },
+        // },
         // {
         //   // 表单项渲染类型 必需
         //   type: 'Select',
@@ -111,6 +141,98 @@ export default {
         //     // labelWidth: '120px',
         //   },
         // },
+        {
+          // 表单项渲染类型 必需
+          type: 'Select',
+          // 表单是否隐藏 默认为false（控制的是el-form-item）
+          hidden: false,
+          // 表单项的配置项
+          attrs: {
+            // 表单项绑定的值（字段名） 必需
+            field: 'city',
+            // 初始值
+            value: 'Guangzhou',
+            clearable: true,
+
+            groupOptions: [
+              {
+                // 分组名称
+                label: '中国城市',
+                options: [
+                  {
+                    // value做key
+                    value: 'Chengdu',
+                    label: '成都',
+                    slot: o => {
+                      const { attr } = o
+                      console.log()
+                      return <span style="float: left">{attr.label}</span>
+                    },
+                  },
+                  {
+                    value: 'Shenzhen',
+                    label: '深圳',
+                    slot: () => {},
+                  },
+                  {
+                    value: 'Guangzhou',
+                    label: '广州',
+                    slot: () => {},
+                  },
+                  {
+                    value: 'Dalian',
+                    label: '大连',
+                    slot: () => {},
+                  },
+                ],
+              },
+            ],
+            // select options
+            // options: [
+            //   {
+            //     value: 'Chengdu',
+            //     label: '成都',
+            //   },
+            //   {
+            //     value: 'Shenzhen',
+            //     label: '深圳',
+            //   },
+            //   {
+            //     value: 'Guangzhou',
+            //     label: '广州',
+            //   },
+            //   {
+            //     value: 'Dalian',
+            //     label: '大连',
+            //   },
+            // ],
+          },
+          // 表单项事件
+          on: {
+            blur: () => {
+              console.log('失去焦点')
+            },
+            input: () => {
+              console.log('input事件')
+            },
+          },
+          // 插槽
+          scopedSlots: {
+            prefix: () => {
+              return '表单内头部内容'
+            },
+            empty: () => {
+              return '空'
+            },
+          },
+          // el-form-item配置项 可选
+          config: {
+            // vue ref属性 默认为field 利用此属性来查找某一组件
+            ref: 'age',
+            label: 'select：',
+            // labelWidth: '120px',
+          },
+        },
       ],
     }
   },
--- src/components/modules/select.tsx
+++ src/components/modules/select.tsx
@@ -1,21 +1,47 @@
 import Vue, { VNode, CreateElement } from 'vue'
 import { Component, Prop, Emit, Watch } from 'vue-property-decorator'
+import omit from 'lodash/omit'
 
 @Component
 export default class SelectPlus extends Vue {
   mounted() {
     console.log(this.$attrs, 'attrs')
     console.log(this.$listeners, 'listeners')
+    console.log(this.$scopedSlots, 'scopedSlots')
   }
 
   render(h: CreateElement): VNode {
-    const renderOptions = () => {
-      const { options } = this.$attrs
+
+    // 组装插槽及作用域插槽
+    const scopedSlots = this.$scopedSlots
+    const slots = []
+    for (let slot in scopedSlots) {
+      // el-input内部使用了 v-if="$slots.[slotName]"来判断是否有插槽
+      // 因此这一步是骗它有插槽，然后再用scopedSlots来实现自定义渲染函数渲染插槽内容
+      slots.push({ name: slot, value: [h('template')] })
+    }
+
+    const renderOptions = (options: any) => {
       console.log(options)
-      return (options as any).map((o: any) => {
-        const { label, value } = o
-        return <el-option key={value} label={label} value={value}></el-option>
+      return options.map((o: any) => {
+        const { value, slot } = o
+        console.log(slot, '567')
+        return <el-option key={value} {...{ props: o }}>{slot({attr: o})}</el-option>
       })
+    }
+    const renderGroupOption = () => {
+      const { groupOptions, options } = this.$attrs
+      // groupOptions只要存在，就渲染分组select
+      if (groupOptions) {
+        return (groupOptions as any).map((o: any) => {
+          const { options: gOptions } = o
+          // 除options之外的配置项均为group参数
+          const restAttrs = omit(o, 'options')
+
+          return <el-option-group {...{ props: restAttrs }}>{renderOptions(gOptions)}</el-option-group>
+        })
+      }
+      return renderOptions(options)
     }
     return (
       <elSelect
@@ -25,8 +51,12 @@ export default class SelectPlus extends Vue {
           this.$emit('input', val)
         }}
         {...{ props: this.$attrs, on: this.$listeners }}
+        {...{ scopedSlots }}
       >
-        {renderOptions()}
+        {renderGroupOption()}
+        {slots.map(o => {
+          return <template slot={o.name}>{o.value}</template>
+        })}
       </elSelect>
     )
   }
