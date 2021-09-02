63804bf2eb62f01cc1b1bb8d6f2e422375859029
aimeiyijia <1456723200@qq.com>
2021-09-02 22:10:01 +0800

    调整check-change

--- mock/check-box.ts
+++ mock/check-box.ts
@@ -7,16 +7,34 @@ const checkbox = {
   attrs: {
     // 表单项绑定的值（字段名） 必需
     field: 'checkBox',
+
+    // 只需要渲染一个checkbox时，设置label属性 value初始值为boolean类型
+    // 注意 只要有options此配置项，那么就只能渲染checkbox组了
+    label: '就一个',
+    // value: true,
+    // disabled: true,
+    // 设置全局的checkbox类型，优先级低于options内部的type属性（el-checkbox-button）
+    type: 'button',
     // 初始值
-    value: '',
+    value: [1],
+    options: [
+      {
+        label: '1',
+        value: 1,
+        // 只要这个属性存在,该项就渲染为el-checkbox
+        type: '',
+      },
+      {
+        // 还是 el-checkbox-button
+        label: '2',
+        value: 2,
+      },
+    ],
   },
   // 表单项事件
   on: {
-    blur: () => {
-      console.log('失去焦点')
-    },
-    input: () => {
-      console.log('input事件')
+    change: val => {
+      console.log('groupChange', val)
     },
   },
   // 插槽
@@ -26,7 +44,7 @@ const checkbox = {
     // vue ref属性 默认为field 利用此属性来查找某一组件
     ref: 'checkBox',
     label: 'checkBox：',
-    // labelWidth: '120px',
+    labelWidth: '120px',
   },
 }
 export default checkbox
--- src/components/modules/check-box.tsx
+++ src/components/modules/check-box.tsx
@@ -17,55 +17,50 @@ export default class CheckBoxPlus extends Vue {
 
   render(h: CreateElement): VNode {
     // 取出Checkbox渲染数组
-    const { groupOptions, options, type } = this.$attrs
-    // 获取出除groupOptions, options之外的配置项
-    const attrs = omit(this.$attrs, ['groupOptions', 'options', 'type'])
-    const { groupChange, singleChange } = this.$listeners
+    const { options = [], lable, type = '' } = this.$attrs
+    // 获取出除options, options之外的配置项
+    const attrs = omit(this.$attrs, ['options', 'type', 'lable'])
 
     let CheckboxType = 'el-checkbox'
     if (type) CheckboxType = 'el-checkbox-' + type
 
-    console.log(attrs, '配置项')
     // 单选框
     const renderOptions = () => {
-      return (options as any).map((o: any) => {
-        const { label, value, type } = o
-        const restAttrs = omit(o, ['label', 'value', 'type'])
-        let CheckboxTypeChild = 'el-checkbox'
-        if (type === "button") { CheckboxTypeChild = 'el-checkbox-button' }
-        else if (!type) { CheckboxTypeChild = CheckboxType }
-        else { CheckboxTypeChild = 'el-checkbox' }
-        console.log(restAttrs, '123')
-        return (
-          <CheckboxTypeChild
-            on-input={(val: any) => {
-              this.$emit('input', val, '123456')
-            }}
-            {...{ props: { ...attrs, label: value, ...restAttrs }, on: { change: singleChange } }}
-          >
-            {label}
-          </CheckboxTypeChild>
-        )
-      })
+      const { label, value, type } = attrs
+      const restAttrs = omit(attrs, ['label', 'value', 'type'])
+      let CheckboxTypeChild = 'el-checkbox'
+      if (type === "button") { CheckboxTypeChild = 'el-checkbox-button' }
+      else if (!type) { CheckboxTypeChild = CheckboxType }
+      else { CheckboxTypeChild = 'el-checkbox' }
+      return (
+        <CheckboxTypeChild
+          on-input={(val: any) => {
+            this.$emit('input', val)
+          }}
+          {...{ props: { ...attrs, label: value, ...restAttrs }, on: this.$listeners }}
+        >
+          {label}
+        </CheckboxTypeChild>
+      )
     }
 
     // 单选框组
     const renderGroupOption = () => {
-      const checkboxs = (groupOptions as any).map((o: any) => {
+      const checkboxs = (options as any).map((o: any) => {
         const { label, value, type } = o
         const restAttrs = omit(o, ['label', 'value', 'type'])
         let CheckboxTypeChild = 'el-checkbox'
         if (type === "button") { CheckboxTypeChild = 'el-checkbox-button' }
-        else if (!type) { CheckboxTypeChild = CheckboxType }
+        else if (!o.hasOwnProperty('type')) { CheckboxTypeChild = CheckboxType }
         else { CheckboxTypeChild = 'el-checkbox' }
         return <CheckboxTypeChild {...{ props: { label: value, ...restAttrs } }}>{label}</CheckboxTypeChild>
       })
       return (
         <el-checkbox-group
           on-input={(val: any) => {
-            this.$emit('input', val, '1234')
+            this.$emit('input', val)
           }}
-          {...{ props: { ...attrs }, on: { change: groupChange } }}>
+          {...{ props: { ...attrs }, on: this.$listeners }}>
           {checkboxs}
         </el-checkbox-group>
       )
@@ -73,7 +68,9 @@ export default class CheckBoxPlus extends Vue {
     }
 
     const renderCheckboxs = () => {
-      return renderOptions().concat(renderGroupOption())
+      // return renderOptions().concat(renderGroupOption())
+      if (options.length === 0) return renderOptions()
+      return renderGroupOption()
     }
     return <fragment>{renderCheckboxs()}</fragment>
   }
