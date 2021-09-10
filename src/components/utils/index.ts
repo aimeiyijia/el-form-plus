3c7916f328bb19e3741be6241a73ad9c6c1a890d
aimeiyijia <1456723200@qq.com>
2021-09-11 00:23:46 +0800

    增加工具函数

--- src/components/utils/index.ts
+++ src/components/utils/index.ts
@@ -41,3 +41,47 @@ export const isUndefined = val => {
 export const isDefined = val => {
   return val !== undefined && val !== null
 }
+
+export const isEmpty = function(val) {
+  // null or undefined
+  if (val == null) return true
+
+  if (typeof val === 'boolean') return false
+
+  if (typeof val === 'number') return !val
+
+  if (val instanceof Error) return val.message === ''
+
+  switch (Object.prototype.toString.call(val)) {
+    // String or Array
+    case '[object String]':
+    case '[object Array]':
+      return !val.length
+
+    // Map or Set or File
+    case '[object File]':
+    case '[object Map]':
+    case '[object Set]': {
+      return !val.size
+    }
+    // Plain Object
+    case '[object Object]': {
+      return !Object.keys(val).length
+    }
+  }
+
+  return false
+}
+
+export const kebabCase = function(str) {
+  const hyphenateRE = /([^-])([A-Z])/g
+  return str
+    .replace(hyphenateRE, '$1-$2')
+    .replace(hyphenateRE, '$1-$2')
+    .toLowerCase()
+}
+
+export const capitalize = function(str) {
+  if (!isString(str)) return str
+  return str.charAt(0).toUpperCase() + str.slice(1)
+}
