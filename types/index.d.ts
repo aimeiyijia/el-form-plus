82bde571882dfdfca8df2efffd589df35b2e24a9
aimeiyijia <1456723200@qq.com>
2022-02-21 23:01:30 +0800

    v0.0.3

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,1 +1,10 @@
 
+import Vue, { Component } from 'vue'
+
+interface IOptionComponent {
+  [proppName: string]: Component
+}
+export interface InstallationOptions {
+  components?: IOptionComponent
+}
+export function install(vue: typeof Vue, options: InstallationOptions): void
