b304ba5549b41c4e1b94db2c11d3cb0e3244e8c0
aimeiyijia <1456723200@qq.com>
2022-08-29 10:46:17 +0800

    chore：删除内置button中无用代码

--- src/components/custom/button.ts
+++ src/components/custom/button.ts
@@ -1,5 +1,4 @@
 import Vue, { VNode, CreateElement } from 'vue'
-import { Fragment } from 'vue-fragment'
 import { Form } from 'element-ui'
 
 interface IButtonsConfig {
@@ -12,7 +11,6 @@ interface IButtonsConfig {
 }
 
 function findElFormComponent(instance: Vue): any {
-  console.log(instance, '实例')
   const componentName = 'ElForm'
   let parent = instance.$parent || instance.$root;
   let name = (parent.$options as any).componentName;
@@ -25,7 +23,6 @@ function findElFormComponent(instance: Vue): any {
     }
   }
   if (parent) {
-    console.log(parent, '父组件')
     return parent
   }
   return false
@@ -43,6 +40,9 @@ function isRenderConfirmButton(
   return confirm ?
     h('el-button',
       {
+        props: {
+          type: "primary"
+        },
         on: {
           click: () => {
             const elForm = findElFormComponent(instance)
@@ -76,7 +76,6 @@ function isRenderResetButton(
     h('el-button', {
       on: {
         click: () => {
-          console.log(instance)
           const elForm = findElFormComponent(instance)
           if (elForm) {
             elForm.resetFields()
@@ -86,17 +85,6 @@ function isRenderResetButton(
     }, resetText)
     : ''
 }
-// // 是否渲染取消按钮
-// function isRenderCancleButton(
-//   h: CreateElement,
-//   config: {
-//     cancel?: boolean
-//     cancelText?: string
-//   },
-//   instance: Form) {
-//   const { cancel = true, cancelText = '取消' } = config
-//   return cancel ? h('el-button', {}, cancelText) : ''
-// }
 
 function renderButtons(config: IButtonsConfig) {
   // 默认的提交 重置按钮
@@ -105,37 +93,6 @@ function renderButtons(config: IButtonsConfig) {
     // 表单项渲染类型 必需
     type: 'Custom',
     field: 'ButtonCustom',
-    // on: {
-    //   click: ({ instance }: { instance: Vue }) => {
-    //     const elForm = findElFormComponent(instance)
-    //     if (elForm) {
-    //       elForm.validate((valid: boolean) => {
-    //         if (valid) {
-    //           alert('submit!');
-    //           return true
-    //         } else {
-    //           alert('error submit!!');
-    //           return false;
-    //         }
-    //       });
-    //     }
-    //   }
-    // },
-    // more: [
-    //   {
-    //     type: 'Button',
-    //     field: 'reset',
-    //     value: '重置',
-    //     on: {
-    //       click: ({ instance }: { instance: Vue }) => {
-    //         const elForm = findElFormComponent(instance)
-    //         if(elForm){
-    //           elForm.resetFields();
-    //         }
-    //       },
-    //     },
-    //   },
-    // ],
     scopedSlots: {
       // jsx或h
       // 这样就可以实现自定义组件的双向绑定
@@ -145,7 +102,6 @@ function renderButtons(config: IButtonsConfig) {
         return h('Fragment', [
           isRenderConfirmButton(h, config, instance),
           isRenderResetButton(h, config, instance),
-          // isRenderCancleButton(h, config, instance)
         ])
       },
     },
