fdb0ee875ba498119a8c6dafa2476a10a41cb16e
aimeiyijia <1456723200@qq.com>
2022-12-14 09:03:52 +0800

    chore：调整组件名称

--- src/components/modules/index.tsx
+++ src/components/modules/index.tsx
@@ -1,43 +1,43 @@
-import AutocompletePlus from './autocomplete'
-import CascaderPlus from './cascader'
-import CascaderPanelPlus from './cascader-panel'
-import CheckBoxPlus from './check-box'
-import ColorPickerPlus from './color-picker'
-import CustomPlus from './custom'
-import DatePickerPlus from './date-picker'
-import DateTimePickerPlus from './date-time-picker'
-import InputNumberPlus from './input-number'
-import InputPlus from './input'
-import RadioPlus from './radio'
-import RatePlus from './rate'
-import SelectPlus from './select'
-import SliderPlus from './slider'
-import SuperCustomPlus from './super-custom'
-import SwitchPlus from './switch'
-import TimePickerPlus from './time-picker'
-import TimeSelectrPlus from './time-select'
-import TransferPlus from './transfer'
-import UploadPlus from './upload'
+import AutocompleteBase from './autocomplete'
+import CascaderBase from './cascader'
+import CascaderPanelBase from './cascader-panel'
+import CheckBoxBase from './check-box'
+import ColorPickerBase from './color-picker'
+import CustomBase from './custom'
+import DatePickerBase from './date-picker'
+import DateTimePickerBase from './date-time-picker'
+import InputNumberBase from './input-number'
+import InputBase from './input'
+import RadioBase from './radio'
+import RateBase from './rate'
+import SelectBase from './select'
+import SliderBase from './slider'
+import SuperCustomBase from './super-custom'
+import SwitchBase from './switch'
+import TimePickerBase from './time-picker'
+import TimeSelectrBase from './time-select'
+import TransferBase from './transfer'
+import UploadBase from './upload'
 
 export {
-  AutocompletePlus,
-  CascaderPlus,
-  CascaderPanelPlus,
-  CheckBoxPlus,
-  ColorPickerPlus,
-  CustomPlus,
-  DatePickerPlus,
-  DateTimePickerPlus,
-  InputNumberPlus,
-  InputPlus,
-  RadioPlus,
-  RatePlus,
-  SelectPlus,
-  SliderPlus,
-  SuperCustomPlus,
-  SwitchPlus,
-  TimePickerPlus,
-  TimeSelectrPlus,
-  TransferPlus,
-  UploadPlus,
+  AutocompleteBase,
+  CascaderBase,
+  CascaderPanelBase,
+  CheckBoxBase,
+  ColorPickerBase,
+  CustomBase,
+  DatePickerBase,
+  DateTimePickerBase,
+  InputNumberBase,
+  InputBase,
+  RadioBase,
+  RateBase,
+  SelectBase,
+  SliderBase,
+  SuperCustomBase,
+  SwitchBase,
+  TimePickerBase,
+  TimeSelectrBase,
+  TransferBase,
+  UploadBase,
 }
