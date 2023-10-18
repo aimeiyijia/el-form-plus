492f38adfff9cb12aaa114c2defb15a0390d2874
aimeiyijia <1456723200@qq.com>
2023-10-18 15:40:51 +0800

    fix：修复custom组件 值未变化时 触发了表单校验

--- src/components/modules/custom.tsx
+++ src/components/modules/custom.tsx
@@ -1,6 +1,7 @@
 import Vue, { VNode, CreateElement } from 'vue'
 import { Component, Prop, Emit, Watch } from 'vue-property-decorator'
 import { Fragment } from 'vue-frag'
+import isEqual from 'is-equal'
 
 import { isFunction } from '../utils/index'
 
@@ -13,29 +14,38 @@ interface IAttrs {
   components: { Fragment },
 })
 export default class CustomPlus extends Vue {
+  get customValue() {
+    return this.$attrs.value
+  }
 
-  @Watch('$attrs.value', { immediate: true, deep: true })
-  private dataChange(val: any) {
-    this.dispatch('ElFormItem', 'el.form.change', [val])
+  @Watch('customValue', { deep: true })
+  private dataChange(val: any, oldVal: any) {
+    if(!isEqual(val, oldVal)){
+      this.dispatch('ElFormItem', 'el.form.change', [val])
+    }
   }
 
   private dispatch(componentName: string, eventName: string, params: any[]) {
-    let parent = this.$parent || this.$root;
-    let name = (parent.$options as any).componentName;
+    let parent = this.$parent || this.$root
+    let name = (parent.$options as any).componentName
 
     while (parent && (!name || name !== componentName)) {
       // @ts-ignore
-      parent = parent.$parent;
+      parent = parent.$parent
 
       if (parent) {
-        name = (parent.$options as any).componentName;
+        name = (parent.$options as any).componentName
       }
     }
     if (parent) {
       const arg: any = [eventName].concat(params)
       // eslint-disable-next-line prefer-spread
-      parent.$emit.apply(parent, arg);
+      parent.$emit.apply(parent, arg)
     }
+  }
+
+  created() {
+    console.log('组件创建了------')
   }
 
   render(h: CreateElement): VNode {
